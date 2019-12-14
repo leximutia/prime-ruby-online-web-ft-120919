6f4afc4e1f71f91558d8694ef280dd630ddcde35
Lexi Mutia <muse.malize@gmail.com>
2019-12-14 17:55:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,16 @@
 def prime?(num)
-  
-  if num <= 1 
+
+  if num <= 1
     false
-    
+
   else
-    
+
     (2...num).to_a.each { |test|
-      return false if num % test == 0 
+      return false if num % test == 0
     }
-    
+
     return true
-    
-  end 
-  
+
+  end
+
 end
